cd2f7115410798680be261372449209f95e62d6e
Yogi Patel <yogesh.patel@mac.com>
2018-02-11 14:39:40 -0800

    functioning

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+    DROP DATABASE IF EXISTS burgers_db;
+
+
+    CREATE DATABASE burgers_db;
+
+    USE burgers_db;
+
+    create table burgers (
+      id int(20) NOT NULL AUTO_INCREMENT,
+	    burger_name varchar(255) NOT NULL,
+	    devoured BOOLEAN DEFAULT false,
+	    PRIMARY KEY (id)
+        );
+
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Tandori Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Tika Masala Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Dosa Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Tandori Veggie Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Saag Paneer Mushroom Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Biryani Burger', true);
